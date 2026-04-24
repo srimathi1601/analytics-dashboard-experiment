f5f7f0d5f8ab768b95665f84e19350f632b473f3
srimathi1601 <ssrimathi1601@gmail.com>
2026-04-24 10:36:15 +0530

    ga4 file modified

--- lib/ga4.ts
+++ lib/ga4.ts
@@ -5,7 +5,6 @@ const client = new BetaAnalyticsDataClient({
     type: 'service_account',
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    // .env stores \n as literal backslash-n; convert to real newlines
     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
